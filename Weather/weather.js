19ccfef311ea2860ecc4672de6aadb253cc58e77
thomasbryk <thomasbryk@cmail.carleton.ca>
2019-03-11 15:25:42 -0400

    Weather now completely works

--- Weather/weather.js
+++ Weather/weather.js
@@ -10,7 +10,8 @@ function getWeather() {
     var consumer_key = 'dj0yJmk9UlJsbnBQTks0SGdQJnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PWFm';
     var consumer_secret = 'c25c0605b09e1f4502e285b97615d33ed4e85227';
     var concat = '&';
-    var query = { 'location': 'ottawa,on', 'format': 'json', 'u': 'c' };
+    var units = 'c';
+    var query = { 'location': 'ottawa,on', 'format': 'json', 'u': units };
     var oauth = {
         'oauth_consumer_key': consumer_key,
         'oauth_nonce': Math.random().toString(36).substring(2),
@@ -48,30 +49,30 @@ function getWeather() {
         success: function (weather) {
             console.log(weather);
             html = '<h2><i class="icon-' + weather.current_observation.condition.code + '"><span style="font-size: 30px;">&nbsp;</span></i>';
-            if (weather.temp > 30) {
-                html += '<span class="hot"> ' + weather.temp + '&deg;' + weather.units.temp + '</span></h2>';
-            } else if (weather.temp < -10) {
-                html += '<span class="cold"> ' + weather.temp + '&deg;' + weather.units.temp + '</span></h2>';
+            if (weather.current_observation.condition.temperature > 30) {
+                html += '<span class="hot"> ' + weather.current_observation.condition.temperature + '&deg;' + units + '</span></h2>';
+            } else if (weather.current_observation.condition.temperature < -10) {
+                html += '<span class="cold"> ' + weather.current_observation.condition.temperature + '&deg;' + units + '</span></h2>';
             } else {
-                html += '' + weather.temp + '&deg;' + weather.units.temp + '</h2>';
+                html += '' + weather.current_observation.condition.temperature + '&deg;' + units + '</h2>';
             }
             html += '<span class=info>';
-            html += '<li class="city">' + weather.city + ', ' + weather.region + '</li>';
-            html += '<li class="forecastDescription">' + 'Currently ' + weather.currently + ' and feels like ' + ((weather.wind.chill - 32) * (5 / 9) | 0) + '&deg;' + weather.units.temp + '. Todays forecast is ' + weather.forecast[0].text + '.</li>';
-            if ((weather.sunrise).charAt(3) == ' ') {
-                weather.sunrise = weather.sunrise.slice(0, 2) + "0" + weather.sunrise.slice(2);
+            html += '<li class="city">' + weather.location.city + ', ' + weather.location.region + '</li>';
+            html += '<li class="forecastDescription">' + 'Currently ' + weather.current_observation.condition.text + ' and feels like ' + weather.current_observation.wind.chill + '&deg;' + units + '. Todays forecast is ' + weather.forecasts[0].text + '.</li>';
+            if ((weather.current_observation.astronomy.sunrise).charAt(3) == ' ') {
+                weather.current_observation.astronomy.sunrise = weather.current_observation.astronomy.sunrise.slice(0, 2) + "0" + weather.current_observation.astronomy.sunrise.slice(2);
             }
-            if ((weather.sunset).charAt(3) == ' ') {
-                weather.sunset = weather.sunset.slice(0, 2) + "0" + weather.sunset.slice(2);
+            if ((weather.current_observation.astronomy.sunset).charAt(3) == ' ') {
+                weather.current_observation.astronomy.sunset = weather.current_observation.astronomy.sunset.slice(0, 2) + "0" + weather.current_observation.astronomy.sunset.slice(2);
             }
-            html += '<li style="font-size:14pt;"><i2 class="icon-' + 34 + '"></i2> ' + weather.sunrise + '';
-            html += '&nbsp&nbsp&nbsp <i2 class="icon-' + 31 + '"></i2> ' + weather.sunset + '</li>';
+            html += '<li style="font-size:14pt;"><i2 class="icon-' + 34 + '"></i2> ' + weather.current_observation.astronomy.sunrise + '';
+            html += '&nbsp&nbsp&nbsp <i2 class="icon-' + 31 + '"></i2> ' + weather.current_observation.astronomy.sunset + '</li>';
             html += '</span>';
 
             html += '<span class=week>';
             html += '<table style="height:100%;">';
             for (var i = 0; i < 5; i++) {
-                html += '<tr><td class=weekTD><li style="opacity:' + 1 / (i * 1.5) + '; font-family:Roboto-Light;"> ' + weather.forecast[i].day + '</td><td class=weekTD><li style="opacity:' + 1 / (i * 1.5) + '">&nbsp&nbsp' + weather.forecast[i].high + '&deg;' + weather.units.temp + '</td><td class=weekTD><li style="opacity:' + 1 / (i * 1.5) + '">&nbsp<i2 class="icon-' + weather.forecast[i].code + '"></i2></td></tr></li>';
+                html += '<tr><td class=weekTD><li style="opacity:' + 1 / (i * 1.5) + '; font-family:Roboto-Light;"> ' + weather.forecasts[i].day + '</td><td class=weekTD><li style="opacity:' + 1 / (i * 1.5) + '">&nbsp&nbsp' + weather.forecasts[i].high + '&deg;' + units + '</td><td class=weekTD><li style="opacity:' + 1 / (i * 1.5) + '">&nbsp<i2 class="icon-' + weather.forecasts[i].code + '"></i2></td></tr></li>';
             }
             html += '</table></span>';
             $("#weather").html(html);
