5a2ee9c42f884d2e11e23b733f61a40075206c9c
thomasbryk <thomasbryk@cmail.carleton.ca>
2019-07-15 18:50:06 -0400

    Testing new fonts

--- Weather/weather.js
+++ Weather/weather.js
@@ -2,6 +2,7 @@ var API_KEY = '0059287a1f10d4f818f22bc07882e6ae';
 
 
 $(document).ready(function() {
+    html = '<h2><i class="wi wi-owm-200"><span style="font-size: 30px;">&nbsp;</span></i>';
     getWeather(); //Get the initial weather.
     setInterval(getWeather, 600000); //Update the weather every 10 minutes.
 });
