66f2edb8f3c33bebc8f331c588a300de4a39117a
Thomas Bryk <thomasbryk@cmail.carleton.ca>
2019-10-08 22:35:54 -0400

    Test 7

--- Weather/weather.js
+++ Weather/weather.js
@@ -148,7 +148,7 @@ function convertUnixTime(unix_timestamp) {
     var hours = date.getHours(); // Hours part from the timestamp  
     var minutes = "0" + date.getMinutes(); // Minutes part from the timestamp
 
-    return hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2); // Will display time in 10:30:23 format
+    return hours + ':' + minutes.substr(-2); // Will display time in 10:30 format
 }
 
 var weatherIcons = {
